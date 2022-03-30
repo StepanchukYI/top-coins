0cbd1d7ad3fcdd88f30f519fc927747c7cb193c6
evgeniy.stepanchuk <bodunjo855@gmail.com>
2022-03-30 17:13:05 +0300

    fk

--- internal/server/http/router.go
+++ internal/server/http/router.go
@@ -1,7 +1,9 @@
 package utilhttp
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/StepanchukYI/top-coin/internal/services"
 	// services "github.com/StepanchukYI/top-coin/internal/services"
@@ -9,15 +11,25 @@ import (
 
 func (server *Server) Hello(s *services.ApiService) HandlerDesc {
 	h := func(w http.ResponseWriter, r *http.Request) {
+		out := make(chan interface{})
 		ctx := r.Context()
 
-		v, err := s.Hello(ctx)
-		if err != nil {
-			server.createErrorResponse(w, http.StatusInternalServerError, []string{err.Error()})
-			return
-		}
+		go func() {
+			v, err := s.Hello(ctx)
+			if err != nil {
+				server.createErrorResponse(w, http.StatusInternalServerError, []string{err.Error()})
+				return
+			}
+			out <- v
+		}()
 
-		server.createSuccessResponse(w, http.StatusOK, v)
+		select {
+		case <-ctx.Done():
+			fmt.Fprintf(os.Stderr, "request cancelled: %v\n", ctx.Err())
+		case v := <-out:
+			fmt.Fprintf(os.Stderr, "request SUCCESS:\n")
+			server.createSuccessResponse(w, http.StatusOK, v)
+		}
 
 	}
 
